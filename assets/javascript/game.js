61cd92c245abe7e39438300cf6a2fcb754a6d6a1
soggytoast111 <joncurto@gmail.com>
2019-03-16 00:51:49 -0400

    version 1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,9 +10,14 @@ var selectedchar = 0
 var selectedenemy = 0
 
 //vars for character stats
-var charHealth = [0, 100, 100, 100, 100]
-var charAtk = [0, 10, 10, 10, 10]
-var charEA = [0, 5, 5, 5, 5]
+var charHealth = [0, 750, 500, 100, 1000]
+var charAtk = [0, 25, 20, 5, 1]
+var charEA = [0, 20, 25, 17, 1]
+var attackgrow = [0, 2, 3, 10, 2]
+
+
+//player attack
+var playerattack = 1
 
 //player health
 var lifePoint = 1
@@ -24,222 +29,249 @@ var enemyPoint = 1
 var chararray = [0, character1, character2, character3, character4]
 
 writeText("Select your character!")
+healthint()
+atkinit()
+definit()
 
 //On Click events for all divs
 character1.click(function(){
     if (state == 0) {
     character1.animate({left: '407px'});
-    character2.animate({top: '300px', left: '407px'});
-    character3.animate({top: '300px', left: '100px'});
-    character4.animate({top: '300px', left: '714px'});
+    character2.animate({top: '400px', left: '407px'});
+    character3.animate({top: '400px', left: '100px'});
+    character4.animate({top: '400px', left: '714px'});
     selectedchar = 1
+    setplayerattack()
     state++
     lifePoint = charHealth[selectedchar]
     writeText("Select your enemy!")
+    playcharsound("king","chosen")
     }    
 
     else if (state == 1 && selectedchar == 2) {
-    character3.animate({top: '600px', left: '100px'});
-    character4.animate({top: '600px', left: '325px'});
+    character3.animate({top: '700px', left: '100px'});
+    character4.animate({top: '700px', left: '325px'});
     writeText("Fight!")
     selectedenemy = 1
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("king","chosen")
     }
     
     else if (state == 1 && selectedchar == 3) {
-    character2.animate({top: '600px', left: '100px'});
-    character4.animate({top: '600px', left: '325px'});
+    character2.animate({top: '700px', left: '100px'});
+    character4.animate({top: '700px', left: '325px'});
     character1.animate({left: '407px'});
     writeText("Fight!")
     selectedenemy = 1
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("king","chosen")
     }
     
     else if (state == 1 && selectedchar == 4) {
-    character2.animate({top: '600px', left: '100px'});
-    character3.animate({top: '600px', left: '325px'});
+    character2.animate({top: '700px', left: '100px'});
+    character3.animate({top: '700px', left: '325px'});
     character1.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 1
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("king","chosen")
     }
 
     else if (state == 3|| state == 5  && selectedchar != 1) {
-    character1.animate({top: '300px', left: '407px'});
+    character1.animate({top: '400px', left: '407px'});
     writeText("Fight!")
     selectedenemy = 1
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("king","chosen")
     }
 })
 
 character2.click(function(){
     if (state == 0) {
     character2.animate({left: '407px'});
-    character1.animate({top: '300px', left: '407px'});
-    character3.animate({top: '300px', left: '100px'});
-    character4.animate({top: '300px', left: '714px'});
+    character1.animate({top: '400px', left: '407px'});
+    character3.animate({top: '400px', left: '100px'});
+    character4.animate({top: '400px', left: '714px'});
     selectedchar = 2
+    setplayerattack()
     lifePoint = charHealth[selectedchar]
     state++
     writeText("Select your enemy!")
+    playcharsound("trump","chosen")
     }   
 
     else if (state == 1 && selectedchar == 1) {
-    character3.animate({top: '600px', left: '100px'});
-    character4.animate({top: '600px', left: '325px'});
+    character3.animate({top: '700px', left: '100px'});
+    character4.animate({top: '700px', left: '325px'});
     writeText("Fight!")
     selectedenemy = 2
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("trump","chosen")
     }
 
     else if (state == 1 && selectedchar == 3) {
-    character1.animate({top: '600px', left: '100px'});
-    character4.animate({top: '600px', left: '325px'});
+    character1.animate({top: '700px', left: '100px'});
+    character4.animate({top: '700px', left: '325px'});
     writeText("Fight!")
     selectedenemy = 2
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("trump","chosen")
     }
 
     else if (state == 1 && selectedchar == 4) {
-    character1.animate({top: '600px', left: '100px'});
-    character3.animate({top: '600px', left: '325px'});
+    character1.animate({top: '700px', left: '100px'});
+    character3.animate({top: '700px', left: '325px'});
     writeText("Fight!")
     selectedenemy = 2
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("trump","chosen")
     }
 
     else if (state == 3|| state == 5  && selectedchar != 2) {
-    character2.animate({top: '300px', left: '407px'});
+    character2.animate({top: '400px', left: '407px'});
     writeText("Fight!")
     selectedenemy = 2
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("trump","chosen")
     }
 })
 
 character3.click(function(){
     if (state == 0) {
-    character3.animate({left: '407px', top: '50px'});
-    character2.animate({top: '300px', left: '407px'});
-    character1.animate({top: '300px', left: '100px'});
-    character4.animate({top: '300px', left: '714px'});
+    character3.animate({left: '407px', top: '150px'});
+    character2.animate({top: '400px', left: '407px'});
+    character1.animate({top: '400px', left: '100px'});
+    character4.animate({top: '400px', left: '714px'});
     selectedchar = 3
+    setplayerattack()
     lifePoint = charHealth[selectedchar]
     state++
     writeText("Select your enemy!")
+    playcharsound("squid","chosen")
     }    
 
     else if (state == 1 && selectedchar == 2) {
-    character1.animate({top: '600px', left: '100px'});
-    character4.animate({top: '600px', left: '325px'});
+    character1.animate({top: '700px', left: '100px'});
+    character4.animate({top: '700px', left: '325px'});
     character3.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 3
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("squid","chosen")
     }
         
     else if (state == 1 && selectedchar == 1) {
-    character2.animate({top: '600px', left: '100px'});
-    character4.animate({top: '600px', left: '325px'});
+    character2.animate({top: '700px', left: '100px'});
+    character4.animate({top: '700px', left: '325px'});
     character3.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 3
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("squid","chosen")
     }
         
     else if (state == 1 && selectedchar == 4) {
-    character1.animate({top: '600px', left: '100px'});
-    character2.animate({top: '600px', left: '325px'});
+    character1.animate({top: '700px', left: '100px'});
+    character2.animate({top: '700px', left: '325px'});
     character3.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 3
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("squid","chosen")
     }
 
     else if (state == 3|| state == 5  && selectedchar != 3) {
-    character3.animate({top: '300px', left: '407px'});
+    character3.animate({top: '400px', left: '407px'});
     writeText("Fight!")
     selectedenemy = 3
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("squid","chosen")
     }
 
 })
 
 character4.click(function(){
     if (state == 0) {
-    character4.animate({left: '407px', top: '50px'});
-    character2.animate({top: '300px', left: '407px'});
-    character1.animate({top: '300px', left: '100px'});
-    character3.animate({top: '300px', left: '714px'});
+    character4.animate({left: '407px', top: '150px'});
+    character2.animate({top: '400px', left: '407px'});
+    character1.animate({top: '400px', left: '100px'});
+    character3.animate({top: '400px', left: '714px'});
     selectedchar = 4
+    setplayerattack()
     lifePoint = charHealth[selectedchar]
     state++
     writeText("Select your enemy!")
+    playcharsound("neg","chosen")
     }
 
     else if (state == 1 && selectedchar == 2) {
-    character3.animate({top: '600px', left: '100px'});
-    character1.animate({top: '600px', left: '325px'});
+    character3.animate({top: '700px', left: '100px'});
+    character1.animate({top: '700px', left: '325px'});
     character4.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 4
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("neg","chosen")
     }
             
     else if (state == 1 && selectedchar == 1) {
-    character3.animate({top: '600px', left: '100px'});
-    character2.animate({top: '600px', left: '325px'});
+    character3.animate({top: '700px', left: '100px'});
+    character2.animate({top: '700px', left: '325px'});
     character4.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 4
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("neg","chosen")
     }
             
     else if (state == 1 && selectedchar == 3) {
-    character1.animate({top: '600px', left: '100px'});
-    character2.animate({top: '600px', left: '325px'});
+    character1.animate({top: '700px', left: '100px'});
+    character2.animate({top: '700px', left: '325px'});
     character4.animate({left: '407px'})
     writeText("Fight!")
     selectedenemy = 4
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("neg","chosen")
     }
 
     else if (state == 3|| state == 5 && selectedchar != 4) {
-    character4.animate({top: '300px', left: '407px'});
+    character4.animate({top: '400px', left: '407px'});
     writeText("Fight!")
     selectedenemy = 4
     enemyPoint = charHealth[selectedenemy]
     state++
     atkshow()
+    playcharsound("neg","chosen")
     }
 })
 
@@ -254,12 +286,14 @@ function killEnemy1() {
     character1.hide()
     character1.animate({top: '2000px', right: '2000px'})
     state++
+    playcharsound("king", "die")
     writeText("Enemy defeated!  Who to fight next?")
     }
 
     else if (state == 6) {
     character1.hide()
     character1.animate({top: '2000px', right: '2000px'})
+    playcharsound("king", "die")
     winGame()
     }
 
@@ -270,12 +304,14 @@ function killEnemy2() {
     character2.hide()
     character2.animate({top: '2000px', right: '2000px'})
     state++
+    playcharsound("trump", "die")
     writeText("Enemy defeated!  Who to fight next?")
     }
 
     else if (state == 6) {
     character2.hide()
     character2.animate({top: '2000px', right: '2000px'})
+    playcharsound("trump", "die")
     winGame()
     }
 
@@ -286,12 +322,14 @@ function killEnemy3() {
     character3.hide()
     character3.animate({top: '2000px', right: '2000px'})
     state++
+    playcharsound("squid", "die")
     writeText("Enemy defeated!  Who to fight next?")
     }
 
     else if (state == 6) {
     character3.hide()
     character3.animate({top: '2000px', right: '2000px'})
+    playcharsound("squid", "die")
     winGame()
     }
     
@@ -303,12 +341,14 @@ function killEnemy4() {
     character4.hide()
     character4.animate({top: '2000px', right: '2000px'})
     state++
+    playcharsound("neg", "die")
     writeText("Enemy defeated!  Who to fight next?")
     }
 
     else if (state == 6) {
     character4.hide()
     character4.animate({top: '2000px', right: '2000px'})
+    playcharsound("neg", "die")
     winGame()
     }
 
@@ -317,14 +357,25 @@ function killEnemy4() {
 //Attack Button (The real one!)
 $("#attack1").click(function() {
     attackanimation();
+    playcharsound(charpickarray[selectedchar], "attack")
     setTimeout(function() {damageanimation();}, 500)
+    setTimeout(function() {playatksound();}, 50)
     setTimeout(function() {defanimation();}, 1250)
+    setTimeout(function() {playatksound();}, 1250)
     setTimeout(function() {hitanimation();}, 1750)
     attack()
     atkDisp()
+    atkcalc()
+    atkgrow()
+    if (enemyPoint <=0) {
+        killarray[selectedenemy]()
+    }
+
 })
 
 //kill buttons (debug/dev purpose only) 
+
+var killarray = [0, killEnemy1, killEnemy2, killEnemy3, killEnemy4]
 $("#killbtn1").click(function() {
     killEnemy1()
     atkhide()
@@ -383,7 +434,7 @@ function loseGame () {
 //Attacking scripts
 function attack() {
 lifePoint = lifePoint - charEA[selectedenemy]
-enemyPoint = enemyPoint - charAtk[selectedchar]
+enemyPoint = enemyPoint - playerattack
 atkDisp()
 }
 
@@ -451,4 +502,80 @@ function atkshow() {
 
 function atkhide() {
     $("#attack1").animate({left: '2000'}, "fast")
+}
+
+
+//music
+var musicarray = ["assets/Sounds/music/music1.mp3", "assets/Sounds/music/music2.mp3"]
+var musicstate = 0
+
+function playmusic() {
+    var rand = Math.floor(Math.random() * musicarray.length);
+    document.getElementById("music").src = musicarray[rand]
+    document.getElementById("music").play()
+    musicstate=1
+}
+
+$("#bodycontainer").click(function() {
+    if (musicstate==0) {
+        playmusic()
+    }
+})
+
+//atk sounds
+var atksounds = ["assets/Sounds/atk/attack.mp3", "assets/Sounds/atk/attack2.mp3"]
+
+function playatksound() {
+    var rand = Math.floor(Math.random() * atksounds.length);
+    document.getElementById("atksound").src = atksounds[rand]
+    document.getElementById("atksound").play()
+}
+
+//char sounds
+function playcharsound(char, type) {
+    document.getElementById("charsound").src = "assets/Sounds/"+char+"/"+type+".mp3"
+    document.getElementById("charsound").play()
+}
+
+var charpickarray = [0, "king", "trump", "squid", "neg"]
+
+
+//Initialize heath
+function healthint() {
+    $("#kingheart").html(charHealth[1])
+    $("#trumpheart").html(charHealth[2])
+    $("#squidheart").html(charHealth[3])
+    $("#negheart").html(charHealth[4])
+}
+
+var heartarray = [0, $("#kingheart"), $("#trumpheart"), $("#squidheart"), $("#negheart")]
+
+function atkcalc() {
+    heartarray[selectedchar].html(lifePoint)
+    heartarray[selectedenemy].html(enemyPoint)
+}
+
+//Initialize attacks
+function atkinit() {
+    $("#kinga").html(charAtk[1])
+    $("#trumpa").html(charAtk[2])
+    $("#squida").html(charAtk[3])
+    $("#nega").html(charAtk[4])
+}
+
+function definit() {
+    $("#kingd").html(charEA[1])
+    $("#trumpd").html(charEA[2])
+    $("#squidd").html(charEA[3])
+    $("#negd").html(charEA[4])
+}
+
+var atkarray = [0, $("#kinga"), $("#trumpa"), $("#squida"), $("#nega")]
+function atkgrow() {
+    playerattack = playerattack + attackgrow[selectedchar]
+    atkarray[selectedchar].html(playerattack)
+}
+
+function setplayerattack() {
+    playerattack = charAtk[selectedchar]
 }
